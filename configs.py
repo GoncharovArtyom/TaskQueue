1cc35fa1cf67a839ada607d3b51a7e7c025bc194
artyom <artyom.goncharov1@gmail.com>
2019-04-28 10:50:01 +0300

    Refactoring.

--- configs.py
+++ configs.py
@@ -1,7 +1,7 @@
 import os
 
-DATABASE_URI = os.environ.get("DATABASE_URI", "postgresql://postgres:1234@localhost:5432/task_queue")
-RABBITMQ_URI = os.environ.get("RABBITMQ_URI", "amqp://user:1234@localhost/")
+DATABASE_URI = os.environ.get("DATABASE_URI")
+RABBITMQ_URI = os.environ.get("RABBITMQ_URI")
 TASK_Q_NAME = "task_q"
 
 assert DATABASE_URI, "Не задана переменная окружения DATABASE_URI для подключения к БД."
